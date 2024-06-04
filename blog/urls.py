b2560d8d5041f55b8655533683dd26ec05d3bc3b
shin0112 <wnslcosltimo12@gmail.com>
2024-06-04 19:34:17 +0900

    [fix] authorization url 이동

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,6 @@ from . import views
 from django.urls import path 
 from django.urls import include
 from rest_framework import routers
-from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 router.register('Post', views.IntruderImage)
@@ -13,6 +12,5 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('api-root/', include(router.urls)),
-    path('api-token-auth/', obtain_auth_token),
     path('js-test/', views.js_test, name='js_test'),
 ]
