898b8f02c99396eda0516d71d4ccc6752a15dbeb
shin0112 <wnslcosltimo12@gmail.com>
2024-06-04 19:32:21 +0900

    [add] author 키 설정

--- blog/model.py
+++ blog/model.py
@@ -1,5 +1,5 @@
 class Post(models.Model):
-    models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
     title = models.CharField(max_length=200) 
     text = models.TextField() 
     created_date = models.DateTimeField(default=timezone.now) 
